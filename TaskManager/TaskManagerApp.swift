d871e109189af0a737545748f19cd573f9ba786a
Nuha Mohamed <nuhasafia123@gmail.com>
2023-08-03 11:37:58 -0700

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Nuha Mohamed on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
